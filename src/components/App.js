6732fc615b32aa400f4c88a81d67745c8de4f8d0
Aliripoll <aripollortega@gmail.com>
2022-11-28 10:21:16 +0100

    add map list

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,39 @@
 
 import '../styles/App.scss';
+import contacts from '../data/contacts.json';
+import { useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(contacts.results);
+
+   const htmlData = data.map((oneContact, index) => {
+    return (
+      <tr key={index}>
+      <td>{oneContact.name}</td>
+      <td>{oneContact.counselor}</td>
+      <td>{oneContact.speciality}</td>
+    </tr>
+    );
+    }); 
+  
   return (
     <div>
-      <h1>Hola mundo</h1>
+      <header className='header'>
+        <h1 className='header__title'>Adalabers</h1>
+      </header>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Tutora</th>
+            <th>Especialidad</th>
+          </tr>
+        </thead>
+        <tbody>
+      {htmlData}
+        </tbody>
+      </table>
+
     </div>
   );
 }
