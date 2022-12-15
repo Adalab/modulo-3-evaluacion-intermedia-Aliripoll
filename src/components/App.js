3320ee772bdeca6023a8267e38982223c5a02e07
Aliripoll <aripollortega@gmail.com>
2022-12-15 13:18:42 +0100

    update useState with defect value

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,45 @@
-
-import '../styles/App.scss';
-import contacts from '../data/contacts.json';
-import { useState } from 'react';
+import "../styles/App.scss";
+import contacts from "../data/contacts.json";
+import { useState } from "react";
 
 function App() {
   const [data, setData] = useState(contacts.results);
-  const  [newContact, setNewContact] = useState();
-  const [search, setSearch] = useState();
+  const [newContact, setNewContact] = useState("");
+  const [search, setSearch] = useState({
+    name: "",
+    counselor: "",
+    speciality: "",
+    social_networks: [],
+  });
 
-   const handleNewContact = (ev) => {
-    setNewContact({...newContact, [ev.target.id]: ev.target.value});
-   };
-   
-   const handleClick = (ev) => {
-    ev.preventDefault()
+  const handleNewContact = (ev) => {
+    setNewContact({ ...newContact, [ev.target.id]: ev.target.value });
+  };
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
     setData([...data, newContact]);
-   };
+  };
 
-   const handleSearch = (ev) => {
+  const handleSearch = (ev) => {
     setSearch(ev.target.value);
-   };
+  };
 
-
-   const htmlData = data.map((oneContact, index) => {
+  const htmlData = data.map((oneContact, index) => {
     return (
       <tr key={index}>
-      <td>{oneContact.name}</td>
-      <td>{oneContact.counselor}</td>
-      <td>{oneContact.speciality}</td>
-    </tr>
+        <td>{oneContact.name}</td>
+        <td>{oneContact.counselor}</td>
+        <td>{oneContact.speciality}</td>
+      </tr>
     );
-    }); 
-  
+  });
+
   return (
     <div>
-      <header className='header'>
-        <h1 className='header__title'>Adalabers</h1>
-      </header> 
+      <header className="header">
+        <h1 className="header__title">Adalabers</h1>
+      </header>
       <form action="">
         <label htmlFor="">Nombre:</label>
         <input
@@ -45,8 +48,8 @@ function App() {
           name="name"
           id="name"
           onInput={handleSearch}
-        />  
-        </form> 
+        />
+      </form>
       <table className="table">
         <thead>
           <tr>
@@ -55,37 +58,33 @@ function App() {
             <th>Especialidad</th>
           </tr>
         </thead>
-        <tbody>
-      {htmlData}
-        </tbody>
+        <tbody>{htmlData}</tbody>
       </table>
-      <section className='section'>
+      <section className="section">
         <h2>Añadir una nueva Adalaber</h2>
         <form>
           <label htmlFor="name">Nombre:</label>
-          <input 
-           type="text"
-           name="name" 
-           id="name"
-           onInput={handleNewContact} />
+          <input type="text" name="name" id="name" onInput={handleNewContact} />
           <label htmlFor="counselor">Tutora:</label>
-          <input 
-          type="text" 
-          name="counselor" 
-          id="counselor"
-          onInput={handleNewContact}/>
-        
+          <input
+            type="text"
+            name="counselor"
+            id="counselor"
+            onInput={handleNewContact}
+          />
+
           <label htmlFor="speciality">Especialidad:</label>
-          <input 
-          type="text" 
-          name="speciality" 
-          id="speciality"
-          onInput={handleNewContact} />
+          <input
+            type="text"
+            name="speciality"
+            id="speciality"
+            onInput={handleNewContact}
+          />
         </form>
-        <button className='section__btn' onClick={handleClick}>Añadir una nueva Adalaber</button>
-
+        <button className="section__btn" onClick={handleClick}>
+          Añadir una nueva Adalaber
+        </button>
       </section>
-
     </div>
   );
 }
